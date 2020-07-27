1acce75a9a8c846d4284c6b83bba717fd0b1d457
ChumanJV <joao.fernandes106@etec.sp.gov.br>
2020-07-27 12:32:11 -0300

    commit atualizacao

--- src/categorias/controle/create-categoria.js
+++ src/categorias/controle/create-categoria.js
@@ -1,35 +1,32 @@
-$(document).ready(function () {
-  $(".btn-save").click(function (e) {
-    e.preventDefault();
+$(document).ready(function() {
+  $('.btn-save').click(function(e) {
+      e.preventDefault()
 
-    let dados = $("#form-categoria").serialize();
+      let dados = $('#form-categoria').serialize()
 
-    $("input[type=checkbox]").each(function () {
-      if (!this.checked) {
-        dados += "&" + this.name + "=off";
-      }
-    });
-
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      assync: true,
-      data: dados,
-      url: "src/categorias/modelo/create-categoria.php",
-      success: function (dados) {
-        Swal.fire({
-          title: "appAulaDS",
-          text: dados.mensagem,
-          type: dados.tipo,
-          confirmButtonText: "OK",
-        }).then((result) => {
-          if (result.value) {
-            $("#conteudo").load("src/categorias/visao/list-categoria.html");
+      $('input[type=checkbox]').each(function() {
+          if (!this.checked) {
+              dados += '&' + this.name + '=off'
           }
-        });
+      })
 
-        $("#modal-categoria").modal("hide");
-      },
-    });
-  });
-});
+      $.ajax({
+          type: 'POST',
+          dataType: 'json',
+          assync: true,
+          data: dados,
+          url: 'src/categorias/modelo/create-categoria.php',
+          success: function(dados) {
+              Swal.fire({
+                  title: 'appAulaDS',
+                  text: dados.mensagem,
+                  type: dados.tipo,
+                  confirmButtonText: 'OK'
+              })
+
+              $('#modal-categoria').modal('hide')
+              $('#table-categoria').DataTable().ajax.reload()
+          }
+      })
+  })
+})
--- src/categorias/modelo/create-categoria.php
+++ src/categorias/modelo/create-categoria.php
@@ -2,44 +2,50 @@
 
 include('../../banco/conexao.php');
 
-if (!$conexao) {
+if(!$conexao){
     $dados = array(
         'tipo' => 'info',
-        'mensagem' => 'Ops, não foi possível obter uma conexão com o banco de dados, tente mais tarde...'
+        'mensagem' => 'OPS, não foi possível obter uma conexão com o banco de dados, tente mais tarde..'
     );
-} else {
+} else{
+
     $requestData = $_REQUEST;
 
-    if (empty($requestData['nome']) || empty($requestData['ativo'])) {
+    if(empty($requestData['nome']) || empty($requestData['ativo']) ){
         $dados = array(
             'tipo' => 'info',
             'mensagem' => 'Existe(m) campo(s) obrigatório(s) vazio(s).'
         );
     } else {
+
+        //$requestData = array_map('utf8_decode', $requestData);
+
         $requestData['ativo'] = $requestData['ativo'] == "on" ? "S" : "N";
+
+        //$requestData['dataagora'] = date('Y-d-m H:i:s', strtotime($requestData['dataagora']));
 
         $date = date_create_from_format('d/m/Y H:i:s', $requestData['dataagora']);
         $requestData['dataagora'] = date_format($date, 'Y-m-d H:i:s');
 
-        $sql = "INSERT INTO CATEGORIAS (nome, ativo, datacriacao, datamodificacao)
-        VALUES ('$requestData[nome]', '$requestData[ativo]', '$requestData[dataagora]', '$requestData[dataagora]')";
+        $sqlComando = "INSERT INTO categorias (nome, ativo, datacriacao, datamodificacao)
+         VALUES ('$requestData[nome]', '$requestData[ativo]', '$requestData[dataagora]', '$requestData[dataagora]')";
 
-        $resultado = mysqli_query($conexao, $sql);
+         $resultado = mysqli_query($conexao, $sqlComando);
 
-        if ($resultado) {
+         if($resultado){
             $dados = array(
                 'tipo' => 'success',
                 'mensagem' => 'Categoria criada com sucesso.'
             );
-        } else {
+         } else{
             $dados = array(
                 'tipo' => 'error',
-                'mensagem' => 'Não foi possível criar a categoria.' . mysqli_error($conexao)
+                'mensagem' => 'Não foi possível criar a categoria.'.mysqli_error($conexao)
             );
-        }
+         }
     }
 
-    mysqli_close($conexao); //Fechando a conexão com o banco
+    mysqli_close($conexao);
 }
 
 echo json_encode($dados, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
